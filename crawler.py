91de05be4102a606caeff43ac167948b49794bbc
root <root@localhost.localdomain>
2014-12-21 13:21:53 +0000

    Addding intial push of the crawler before going to the shops :>

---
+++ crawler.py
@@ -0,0 +1,17 @@
+
+
+
+
+class Crawler:
+	
+	""" Simple crawler class to crawl a site over HTTP(S) with the possibility of using a proxy """
+
+	def __init__(self, name):
+		self.maxCrawlDepth = 0;
+		self.crawledUrls = [];
+
+
+	def crawl(self, depth, currentUrl):
+
+
+	def main(self):
